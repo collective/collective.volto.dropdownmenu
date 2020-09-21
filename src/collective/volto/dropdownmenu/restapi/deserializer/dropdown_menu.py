9581540f62acfc38b4c8ba8a922e04b5698a6cf0
Andrea Cecchi <andrea.cecchi85@gmail.com>
2020-09-21 14:48:31 +0200

    simplified uid mapping

--- src/collective/volto/dropdownmenu/restapi/deserializer/dropdown_menu.py
+++ src/collective/volto/dropdownmenu/restapi/deserializer/dropdown_menu.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
-from Acquisition import aq_parent
 from collective.volto.dropdownmenu.interfaces import IDropDownMenu
-from plone import api
 from plone.restapi.deserializer import json_body
 from plone.restapi.deserializer.controlpanels import (
     ControlpanelDeserializeFromJson,
 )
 from plone.restapi.interfaces import IDeserializeFromJson
-from plone.uuid.interfaces import IUUID
-from plone.uuid.interfaces import IUUIDAware
 from zExceptions import BadRequest
 from zope.component import adapter
 from zope.interface import implementer
