4e9cdf46ae5af9808cc526eb1adf145f52b1aa4f
mamico <mauro.amico@gmail.com>
2022-02-14 15:49:56 +0100

    cache

--- src/collective/volto/dropdownmenu/__init__.py
+++ src/collective/volto/dropdownmenu/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """Init and utils."""
+import logging
 from zope.i18nmessageid import MessageFactory
 
 
 _ = MessageFactory('collective.volto.dropdownmenu')
+logger = logging.getLogger(__name__)
--- src/collective/volto/dropdownmenu/restapi/dropdown_menu/get.py
+++ src/collective/volto/dropdownmenu/restapi/dropdown_menu/get.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
+from collective.volto.dropdownmenu import logger
 from collective.volto.dropdownmenu.interfaces import IDropDownMenu
-from collective.volto.dropdownmenu.restapi.serializer.dropdown_menu import (
-    serialize_data,
-)
+from collective.volto.dropdownmenu.restapi.serializer.dropdown_menu import serialize_data
+import os
 from plone import api
+from plone.memoize import ram
 from plone.restapi.services import Service
 from zope.interface import implementer
 from zope.publisher.interfaces import IPublishTraverse
+
+
+EXPERIMENTAL_CACHE = os.environ.get('DROPDOWNMENU_EXPERIMENTAL_CACHE', False)
+if EXPERIMENTAL_CACHE:
+    logger.info("Using dropdownmenu experimental cache")
+
+
+def cache_key(fun, *args, **kwargs):
+    return (
+        args,
+        frozenset(kwargs.items()),  # WARN: json_data could be too big as cache key
+        ["Anonymous"]  if api.user.is_anonymous() else api.user.get_roles(user=api.user.get_current()),
+        api.portal.get_tool("portal_catalog").getCounter(),
+    )
 
 
 @implementer(IPublishTraverse)
@@ -20,4 +35,7 @@ class DropDownMenuGet(Service):
         )
         if not record:
             return []
-        return serialize_data(json_data=record, show_children=True)
+        if EXPERIMENTAL_CACHE:
+            return ram.cache(cache_key)(serialize_data)(json_data=record, show_children=True)
+        else:
+            return serialize_data(json_data=record, show_children=True)
